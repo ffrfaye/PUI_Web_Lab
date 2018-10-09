49f1f1d9979a30b099a1b739aecb15c7d9443b21
ffrfaye <ffrances812@gmail.com>
2018-10-09 12:30:15 -0400

    adding JS to to do list

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function addNewList(){
-  alert('hello world alert!')
-  console.log('hello world alert')
+function addNewList() {
+  alert('hello world!');
+}
+
+function addListItem() {
+  console.log('hello world');
+  var list=document.getElementById("groceryList");
+  var inputItem=document.getElementById("newListItem");
+  var newItem=document.createElement("li");
+  newItem.appendChild(document.createTextNode(inputItem.value));
+  list.appendChild(newItem);
+}
+
+function deleteListitem(item) {
+  item.parentNode.removeChild(item);
 }
